f74ef59b79b6a10fe6cf74a205cc8c9f88015a2a
Anat Benski Lipman <anat_benski@hotmail.com>
2018-08-06 19:49:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ document.addEventListener("DOMContentloaded", (e) => {
   // invoking a function here will make sure it happens on page load
   // your code here
 }
+const titanic=document.getElementById("titanic")
+
+console.log().innerhtml=""
